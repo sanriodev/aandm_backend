003757250a69e40a3fc049aa42000dab8b479c2f
Matteo Juen <matteo.juen@ematric.com>
2025-08-21 14:06:21 +0200

    feat: wip core and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoDatabaseProviderModule } from './provider/mongo/mongo-provider.module';
-import { UserAuthModule } from '@Personal/user-auth';
-import { AppConfigModule, HealthModule } from '@Personal/common';
+import { UserAuthModule } from '@personal/user-auth';
+import { AppConfigModule, HealthModule } from '@personal/common';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ErrorFilter } from './common/filters/error.filter';
-import { AppConfigService } from '@Personal/common';
+import { AppConfigService } from '@personal/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
