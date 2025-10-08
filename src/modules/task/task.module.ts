d8bb2a05264be2a9d857b47405d371f3d299eaa6
Matteo Juen <matteo.juen@ematric.com>
2025-10-08 14:52:34 +0200

    fix: update task list last modified user when a child task gets created, updated or deleted

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entity/task.entity';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TaskListModule } from '../tasklist/tasklist.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), TaskListModule],
   controllers: [TaskController],
   providers: [TaskService],
   exports: [TaskService],
